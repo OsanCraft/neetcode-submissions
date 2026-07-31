4785d6b453adc15a718520b214f01cc7c07db30a
Faithful Onamusi <feonamusi@gmail.com>
2026-07-31 13:12:32 -0500

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # have 2 pointers go from the last and 2nd last item,
+        #append them on a seperate list
+        #after sorting, replace s with that placeholder list
+
+        l, r = 0 , len(s) - 1
+
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l+ 1, r - 1
+        
+
+        
+
+
+
+
+
