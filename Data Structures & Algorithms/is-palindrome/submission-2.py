bf5a53a20aa521c616dc3348df15236b09402f86
Faithful Onamusi <feonamusi@gmail.com>
2026-08-05 12:45:22 -0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+
+        #initialize pointers
+        l, r = 0, len(s) - 1  
+
+        while l < r:
+            # if both aren't the same return false
+            #else return true
+
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+
+            while l < r and not self.alphaNum(s[r]):
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+
+        
+
+        
+
+        
+
+        
+        
